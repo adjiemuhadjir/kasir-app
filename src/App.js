92e63ae1df565cc6cca72767e3f4ad811e850bba
Muhadjir Fachrurradjie <42125647+adjiemuhadjir@users.noreply.github.com>
2021-11-22 11:14:00 +0700

    second

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h2>Hola</h2>
+          <h2>Hola Holo</h2>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
